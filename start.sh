966e4b30acdd31a7f77140ef6c6811d04422f4c2
Mark Varley <mark@envision-it.co.uk>
2017-06-12 23:38:32 +0200

    Updated start script

--- start.sh
+++ start.sh
@@ -13,11 +13,6 @@ echo Starting udp-tcp Service
 systemctl start udp-tcp.service
 systemctl enable udp-tcp.service
 
-# Start the tcp-udp service
-echo Starting tcp-udp Service
-systemctl start tcp-udp.service
-systemctl enable tcp-udp.service
-
 # Start the web app
 echo Starting web app
 npm start
